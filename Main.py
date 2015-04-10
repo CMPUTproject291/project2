721afd180f45f8a82bb82b4434fb58b79b3fe6b0
jeremykid <jeremykid1992@gmail.com>
2015-04-09 22:07:59 -0600

    fix bug

--- Main.py
+++ Main.py
@@ -23,7 +23,7 @@ def get_random_char():
 
            
 def Createandpopulateadatabase(mode):
-    DB_SIZE = 100000
+    DB_SIZE = 1000
     SEED = 10000000
     
     if mode == 1:
@@ -263,7 +263,7 @@ def Receivetracekeyrange(mode):
                 start = time.time()
                 count = 0
                 for key in db.keys():
-                    if key > lower and key < upper:
+                    if key >= lower and key <= upper:
                         count += 1
                         file.write(key.decode(encoding='UTF-8')+"\n")
                         file.write(db[key].decode(encoding='UTF-8')+"\n\n")
@@ -282,7 +282,7 @@ def Receivetracekeyrange(mode):
                 start = time.time()
                 count = 0
                 for key in db.keys():
-                    if key > lower and key < upper:
+                    if key >= lower and key <= upper:
                         count += 1
                         file.write(key.decode(encoding='UTF-8')+"\n")
                         file.write(db[key].decode(encoding='UTF-8')+"\n\n")
